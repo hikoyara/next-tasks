14723c4bef0a69f5946189a0745c2959dad6bbfb
hikoyara <hikoyara7@icloud.com>
2024-06-17 01:59:17 +0900

    エラーページ 実装

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import Link from "next/link";
 
-const NotFoundPage = () => {
+const ErrorPage = () => {
     return (
         <div className="h-screen flex flex-col justify-center items-center bg-slate-50 text-gray-900">
-            <h1 className="text-8xl font-bold">404</h1>
-            <p className="text-4xl font-medium">Page Not Found</p>
+            <h1 className="text-8xl font-bold">Error</h1>
+            <p className="text-4xl font-medium">Unexpected error occur</p>
             <Link href="/" className="mt-4 text-xl text-blue-600 hover:underline">
                 Go back home
             </Link>
@@ -12,4 +14,4 @@ const NotFoundPage = () => {
     );
 };
 
-export default NotFoundPage;
+export default ErrorPage;
